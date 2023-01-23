e0a8562d7bb6db34d305eb51eb2327bc9638d015
Murli Monahar Gupta <murlimonahargupta@gmail.com>
2023-01-23 22:02:05 +0530

    c

---
+++ profile.cpp
@@ -0,0 +1,8 @@
+# include<stdio.h>
+using namespace std;
+main()
+{
+	printf("Name:- Murli Monahar Gupta\n\n\n\n\n\n\n\n\n");
+	printf("section:- K22FU\n\n\n\n\n");
+	printf("roll:- 02");
+}
---
+++ sum of 2 numbers day 1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+using namespace std ;
+main()
+{
+	int a,b,c;
+	printf("enter any two numbers\n");
+	scanf("%d %d",&a,&b);
+	c=a+b;
+	
+	printf("Sum of two numbers = %d",c);
+}
